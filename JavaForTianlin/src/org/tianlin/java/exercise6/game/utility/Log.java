f8a01e53c0188ea4a94413d707a7631b1f46cc21
danceyat <danceyat@gmail.com>
2015-08-12 12:25:03 +0800

    Add log system including logging to file and standard output.

---
+++ JavaForTianlin/src/org/tianlin/java/exercise6/game/utility/Log.java
@@ -0,0 +1,249 @@
+package org.tianlin.java.exercise6.game.utility;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Log {
+	private static final String TAG = "Logger";
+	private static final DateFormat LOG_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	private static final DateFormat FILE_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd-hhmmss");
+	private static final boolean DEBUG = true;
+	private static final String LEVEL_INFO = "I";
+	private static final String LEVEL_DEBUG = "D";
+	private static final String LEVEL_ERROR = "E";
+
+	private static final String LOG_DIR = "log";
+	private static FileLogger fileLogger = null;
+
+	private static LogType typeForReopen = LogType.Server;
+	private static final int LOG_RETRY_TIMES = 3;
+
+	/*
+	 * Record game info. Always use this to record game routines.
+	 */
+	public static void i(String TAG, String format, Object... args) {
+		log2Screen(buildLogString(LEVEL_INFO, TAG, format, args));
+		log2file(buildLogString(LEVEL_INFO, TAG, format, args));
+	}
+
+	/*
+	 * Record debug message. It's printed to screen only if DEBUG is true.
+	 */
+	public static void d(String TAG, String format, Object... args) {
+		if (DEBUG)
+			log2Screen(buildLogString(LEVEL_DEBUG, TAG, format, args));
+		log2file(buildLogString(LEVEL_DEBUG, TAG, format, args));
+	}
+
+	/*
+	 * Record errors.
+	 */
+	public static void e(String TAG, String format, Object... args) {
+		log2Screen(buildLogString(LEVEL_ERROR, TAG, format, args));
+		log2file(buildLogString(LEVEL_ERROR, TAG, format, args));
+	}
+
+	/*
+	 * Server or client side should initial once in one game.
+	 */
+	public static void init(LogType type) {
+		typeForReopen = type;
+		if (fileLogger != null) {
+			e(TAG, "File logger already initialized!");
+			return;
+		}
+		String fileName = String.format("%s-%s.txt", type.toString(), FILE_DATE_FORMAT.format(new Date()));
+		d(TAG, "Initializing file logger: %s", fileName);
+		fileLogger = new FileLogger(fileName);
+		new Thread(fileLogger).start();
+	}
+
+	/*
+	 * Server or client side must shut logging thread down.
+	 */
+	public static void uninit() {
+		try {
+			/*
+			 * [WORKAROUND] wait a bit time for logging thread to finish work......
+			 */
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e(TAG, "Main thread is interrupted!");
+		}
+		if (fileLogger != null) {
+			d(TAG, "Uninitializing file logger: %s", fileLogger.filename);
+			fileLogger.shutdown = true;
+			fileLogger = null;
+		}
+	}
+
+	public static void runStressTest() {
+		/*
+		 * make sure threads will not fight with each other
+		 */
+		for (int i = 0; i < 5; i++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						for (int i = 1; i <= 50; i++) {
+							Log.i(TAG, "[StressTest] I'm %s, count %d", Thread.currentThread().getName(), i);
+							Thread.sleep(10);
+						}
+						Thread.sleep(100);
+						for (int i = 1; i <= 50; i++) {
+							Log.e(TAG, "[StressTest] I'm %s again, count %d", Thread.currentThread().getName(), i);
+							Thread.sleep(10);
+						}
+					} catch (InterruptedException e) {
+						e(TAG, "[StressTest] %s is interrupted!", Thread.currentThread().getName());
+					}
+					Log.d(TAG, "[StressTest] I'm %s and I'm going to die.", Thread.currentThread().getName());
+				}
+			}).start();
+		}
+
+		try {
+			/*
+			 * sleep 500 ms here to abort file log
+			 */
+			Thread.sleep(500);
+			uninit();
+			/*
+			 * re-open for normal logging
+			 */
+			Thread.sleep(2000);
+			init(typeForReopen);
+			/*
+			 * sleep 500 ms here to prevend closing
+			 */
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e(TAG, "[StressTest] Main thread is interrupted!");
+		}
+	}
+
+	private static class FileLogger implements Runnable {
+		private static final String TAG = "FileLogger";
+		private String filename = null;
+		private BlockingQueue<String> messageQueue = new LinkedBlockingQueue<String>();
+		private boolean shutdown = false;
+
+		public FileLogger(String filename) {
+			this.filename = filename;
+		}
+
+		@Override
+		public void run() {
+			File log = createAndOpenFile();
+			if (log == null) {
+				e(TAG, "Trying to open file %s failed, check your access right please!", filename);
+				fileLogger = null;
+				messageQueue.clear();
+				return;
+			}
+
+			FileWriter writer = null;
+			try {
+				writer = new FileWriter(log);
+				while (!shutdown) {
+					writer.write(messageQueue.take() + "\n");
+				}
+			} catch (InterruptedException | IOException e) {
+				e(TAG, "Error logging to log file, exception: %s!!", e.getMessage());
+				fileLogger = null;
+				messageQueue.clear();
+				return;
+			} finally {
+				try {
+					if (null != writer)
+						writer.close();
+				} catch (IOException e) {
+					e(TAG, "Error closing log file!!");
+					fileLogger = null;
+					messageQueue.clear();
+				}
+			}
+		}
+
+		/*
+		 * Open file for logging. File name is like Server-20150811-061322.txt.
+		 */
+		private File createAndOpenFile() {
+			File dir = new File(LOG_DIR);
+			if (!dir.exists()) {
+				if (!dir.mkdir()) {
+					e(TAG, "Cannot create directory at %s, please check access rights!", dir.getAbsolutePath());
+					return null;
+				}
+				d(TAG, "Create directory at %s", dir.getAbsolutePath());
+			}
+
+			File logFile = new File(dir, filename);
+			if (logFile.exists()) {
+				// should never happens
+				if (logFile.delete()) {
+					d(TAG, "Delete duplicated log file at %s, WTF!!!", logFile.getAbsolutePath());
+				} else {
+					e(TAG, "Cannot delete duplicated log file!!");
+					return null;
+				}
+			}
+
+			try {
+				logFile.createNewFile();
+				i(TAG, "Create log file %s", logFile.getAbsolutePath());
+			} catch (IOException e) {
+				e(TAG, "Cannot create file at %s, please check access rights!", logFile.getAbsolutePath());
+				return null;
+			}
+
+			return logFile;
+		}
+
+		/*
+		 * Run on other thread.
+		 */
+		public void log(String message) {
+			int retry = 0;
+			while (retry++ < LOG_RETRY_TIMES) {
+				if (messageQueue.offer(message))
+					return;
+				log2Screen(buildLogString(LEVEL_DEBUG, TAG, "Failed adding message to queue, retry: %d", retry));
+			}
+			log2Screen(buildLogString(LEVEL_ERROR, TAG, "Failed adding message to queue, it will be ignored."));
+		}
+	}
+
+	/*
+	 * Print the log message on screen.
+	 */
+	private static void log2Screen(String msg) {
+		System.out.println(msg);
+	}
+
+	/*
+	 * Print the log message to log file if it has been initialized.
+	 */
+	private static void log2file(String msg) {
+		if (fileLogger != null) {
+			fileLogger.log(msg);
+		}
+	}
+
+	/*
+	 * Build log message according to level, tag and user string.
+	 */
+	private static String buildLogString(String level, String TAG, String format, Object... args) {
+		StringBuilder builder = new StringBuilder();
+		builder.append('[').append(LOG_DATE_FORMAT.format(new Date())).append("]/").append(level).append("  -  [")
+				.append(TAG).append(']').append(String.format(format, args));
+		return builder.toString();
+	}
+}
